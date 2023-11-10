1070ad263999bb4e020e01867615909f3057e7b4
David Kent <d.kent@iesohealth.com>
2023-11-10 16:19:53 +0000

    Add third subnet and tags

--- variables.tf
+++ variables.tf
@@ -20,6 +20,11 @@ variable "environment" {
   default     = "dev"
 }
 
+variable "billing_code" {
+  type        = string
+  description = "(Required) Billing Code for Network Resources"
+}
+
 variable "cidr_block" {
   type        = string
   description = "(Optional) The CIDR block for the VPC. Default:10.42.0.0/16"
